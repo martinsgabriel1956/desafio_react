e40ce8ed36aa991d6f81b5151696f446a8c87ed6
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-21 20:42:36 -0300

    footer component finished :tada:

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
+import Home from '../../assets/home.svg';
+import Repos from '../../assets/github.svg';
+import Users from '../../assets/users.svg';
+
 export function Footer() {
   return (
-    <h1>Footer</h1>
+   <div className="footer-container">
+     <Link to="/dashboard" className="tab-buttons">
+      <img src={Home} alt="" />
+      <p>Home</p>
+     </Link>
+     <Link to="/repositories" className="tab-buttons">
+      <img src={Repos} alt="" />
+      <p>Repos</p>
+     </Link>
+     <Link to="/followers" className="tab-buttons">
+      <img src={Users} alt="" />
+      <p>Seguidores</p>
+     </Link>
+     <Link to="/followings" className="tab-buttons">
+      <img src={Users} alt="" />
+      <p>Seguindo</p>
+     </Link>
+   </div>
   );
 }

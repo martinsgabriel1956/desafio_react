003e0898b5177ea7b0bb4fce1763fa7e6631ad55
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-21 16:01:19 -0300

    login page finished :tada:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,17 @@
+import Logo from '../assets/logo.svg';
+import ArrowRight from '../assets/arrow-right.svg';
+
+import '../styles/pages/Login.css';
+
 export function Login() {
   return (
-    <h1>Login</h1>
+    <div className="container">
+     <img className="logo" src={Logo} alt="Logo" />
+     <input type="text" placeholder="Usuário" required />
+     <button className="btn-login">
+        Entrar
+        <img src={ArrowRight} alt="Logo" />
+     </button>
+    </div>
   );
 }

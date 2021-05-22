fc863d370bdd6480e89aece75964f4dbc0580bd0
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-22 10:33:44 -0300

    Home page refactored :tada:

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
-import { Header } from "../components/Header";
+import { Link } from 'react-router-dom';
+
+import "../styles/pages/Home.css";
+
 import { Bio } from "../components/Bio";
 import { Footer } from "../components/Footer";
 
-import '../styles/pages/Home.css';
+import Logout from '../assets/log-out.svg';
 
 export function Home() {
   return (
     <>
-      <Header />
+      <header className="navbar">
+        <nav className="nickname">
+          <p>#martinsgabriel1956</p>
+        </nav>
+        <Link to="/" className="logout">
+          <p>Sair</p>
+          <img src={Logout} alt="Sair" />
+        </Link>
+      </header>
       <Bio />
       <Footer />
-    </>    
+    </>
   );
 }

8e4cb3c812f48834b31115ea64d60be01906b929
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-22 12:22:24 -0300

    user profile route setted :tada:

--- src/components/Follow/index.js
+++ src/components/Follow/index.js
@@ -11,7 +11,7 @@ export function Follow() {
     <div className="follow-container">
       <img className="avatar" src={Avatar} alt="Usuário" />
       <p>#martinsgabriel1956</p>
-      <Link to="/dashboard">
+      <Link to="/user/profile">
         <img src={ArrowRight} alt="" />
       </Link>
     </div>

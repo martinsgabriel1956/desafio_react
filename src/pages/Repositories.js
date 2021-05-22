216d078498f9b6cf1ea4a181102c3d18c8e46db0
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-22 10:38:36 -0300

    repos page finished :tada:

--- src/pages/Repositories.js
+++ src/pages/Repositories.js
@@ -1,5 +1,21 @@
+import React from 'react';
+
+import { Header } from '../components/Header';
+import { Repository } from '../components/Repository';
+import { Footer } from "../components/Footer";
+
+import '../styles/pages/Repositories.css'
+
+
 export function Repositories() {
   return (
-    <h1>Repositories</h1>
+    <>
+      <Header />
+      <Repository />
+      <Repository />
+      <Repository />
+      <Repository />
+      <Footer />
+    </>
   );
 }

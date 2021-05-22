ff699c004cca3ffdb216758a83cef78a0d95ac44
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-22 12:44:42 -0300

    user profile finished :tada:

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/pages/Home.css";
+
+import { Bio } from "../components/Bio";
+import { Footer } from "../components/Footer";
+
+import Save from "../assets/log-in.svg";
 
 export function UserProfile() {
   return (
-    <div>
-      <h1>User Profile</h1>
-    </div>
-  )
+    <>
+      <header className="navbar">
+        <nav className="nickname">
+          <p>#martinsgabriel1956</p>
+        </nav>
+        <div className="logout">
+          <p>Salvar</p>
+          <img src={Save} alt="Sair" />
+        </div>
+      </header>
+      <Bio />
+      <Footer />
+    </>
+  );
 }
-
- 

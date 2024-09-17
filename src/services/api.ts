1bd6c37015e43249b4a88cd3766a0ded8d324dbc
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-09-17 15:44:14 -0300

    fix: remove origin option in the axios config

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.github.com/users/",
-  headers: {
-    "Access-Control-Allow-Origin": import.meta.env.VITE_HOST,
-  },
 });

8b9aea58ef8daeffaf1c70b5c1d664e61f7c9a59
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-09-19 16:00:00 -0300

    feat: Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import './style.scss';
+import { LoadIcon } from "../ui/load-icon";
+
+export function Loading() {
+  return (
+    <div className="loading-component">
+      <LoadIcon />
+    </div>
+  )
+}

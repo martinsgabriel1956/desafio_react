a95a8e9ce73391b1ebdc1af8d7e47dd553c5a361
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-09-19 15:58:15 -0300

    refactor: useUser

--- src/@hooks/useUser.ts
+++ src/@hooks/useUser.ts
@@ -2,6 +2,5 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 export function useUser() {
-  const { data, login, logout, error, setError} = useContext(UserContext);
-  return { data, login, logout, error, setError };
+  return useContext(UserContext);
 }

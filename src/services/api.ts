67414e52b5d0a25b5f85ca7f33614df6b0315492
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-09-19 20:19:17 -0300

    refactor: add authorization option in the axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://api.github.com/users/",
+  headers: {
+    'Authorization': `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  }
 });

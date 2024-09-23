75fb08ee859c13efe18f37caa31077584d28f62f
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-09-23 18:20:25 -0300

    feat: redirect user to login page when is doesn't logged

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import './style.scss';
+import { useUser } from "../../@hooks/useUser";
 import { Footer } from "../../components/Footer";
 
 export function DefaultLayout() {
+  const navigate = useNavigate();
+  const { getUsername } = useUser();
+  const username = getUsername();
+
+  if (!username) {
+    navigate("/");
+  }
+
   return (
     <>
       <Outlet />
